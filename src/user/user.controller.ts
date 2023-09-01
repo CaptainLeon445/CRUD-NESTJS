c59f49cff06322bdce694f10ebec09f77128c22d
Dmajor81 <michelledimeji@gmail.com>
2023-09-01 23:18:29 +0100

    controllers

---
+++ src/user/user.controller.ts
@@ -0,0 +1,39 @@
+import {
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Req,
+  } from '@nestjs/common';
+  import { Request } from 'express';
+  
+@Controller("/user")
+export class UserController {
+
+    @Get()
+  getUsers() {
+    return { name: 'Christopher', email: 'christopher@mail.co' };
+  }
+
+  @Post()
+  store(@Req() req: Request) {
+    return req.body;
+  }
+
+  @Patch('/:userId')
+  updateUser(@Req() req: Request) {
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUser(@Param() params: { userId: number }) {
+    return params;
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params;
+  }
+}
