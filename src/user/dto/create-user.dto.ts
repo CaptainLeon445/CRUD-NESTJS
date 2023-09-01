cbbaa33812bb1e2b314d821e6bf9459e60cd0b3e
Dmajor81 <michelledimeji@gmail.com>
2023-09-02 00:55:09 +0100

    Pipe and Validation updated

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsEmail } from "class-validator";
+
 export class CreateUserDTO {
+  @IsString()
   name: string;
+  @IsEmail()
   email: string;
+  @IsString()
+  password: string;
+
 }
