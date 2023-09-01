aa9886d84eb612fadfe4b4c300416ac2eb751395
Dmajor81 <michelledimeji@gmail.com>
2023-09-01 23:39:36 +0100

    autogenerate controllers, providers and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [AppController],
-  imports: [UserModule],
   providers: [AppService],
+  imports: [UserModule],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,31 +9,32 @@ import {
   } from '@nestjs/common';
   import { Request } from 'express';
   
-@Controller("/user")
+
+@Controller('user')
 export class UserController {
 
     @Get()
-  getUsers() {
-    return { name: 'Christopher', email: 'christopher@mail.co' };
-  }
-
-  @Post()
-  store(@Req() req: Request) {
-    return req.body;
-  }
-
-  @Patch('/:userId')
-  updateUser(@Req() req: Request) {
-    return req.body;
-  }
-
-  @Get('/:userId')
-  getUser(@Param() params: { userId: number }) {
-    return params;
-  }
-
-  @Delete('/:userId')
-  deleteUser(@Param() params: { userId: number }) {
-    return params;
-  }
+    getUsers() {
+      return { name: 'Christopher', email: 'christopher@mail.co' };
+    }
+  
+    @Post()
+    store(@Req() req: Request) {
+      return req.body;
+    }
+  
+    @Patch('/:userId')
+    updateUser(@Req() req: Request) {
+      return req.body;
+    }
+  
+    @Get('/:userId')
+    getUser(@Param() params: { userId: number }) {
+      return params;
+    }
+  
+    @Delete('/:userId')
+    deleteUser(@Param() params: { userId: number }) {
+      return params;
+    }
 }
