b091e22a3385737cdcb99b595113d131e3d29824
Dmajor81 <michelledimeji@gmail.com>
2023-09-02 00:06:58 +0100

    updated the providers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,40 +1,41 @@
 import {
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Patch,
-    Post,
-    Req,
-  } from '@nestjs/common';
-  import { Request } from 'express';
-  
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
 
-    @Get()
-    getUsers() {
-      return { name: 'Christopher', email: 'christopher@mail.co' };
-    }
-  
-    @Post()
-    store(@Req() req: Request) {
-      return req.body;
-    }
-  
-    @Patch('/:userId')
-    updateUser(@Req() req: Request) {
-      return req.body;
-    }
-  
-    @Get('/:userId')
-    getUser(@Param() params: { userId: number }) {
-      return params;
-    }
-  
-    @Delete('/:userId')
-    deleteUser(@Param() params: { userId: number }) {
-      return params;
-    }
+  @Get()
+  getUsers() {
+    return this.userService.get();
+  }
+
+  @Post()
+  store(@Req() req: Request) {
+    return this.userService.create(req);
+  }
+
+  @Patch('/:userId')
+  updateUser(@Req() req: Request, @Param() param: { userId: number }) {
+    return this.userService.update(req, param);
+  }
+
+  @Get('/:userId')
+  getUser(@Param() param: { userId: number }) {
+    return this.userService.getUser(param);
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() param: { userId: number }) {
+    return this.userService.deleteUser(param);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  get() {
+    return { name: 'Christopher', email: 'christopher@mail.co' };
+  }
+
+  create(req: Request) {
+    return req.body;
+  }
+
+  update(req: Request, param: { userId: number }) {
+    return { body: req.body, param };
+  }
+
+  getUser(param: { userId: number }) {
+    return param;
+  }
+  deleteUser(param: { userId: number }) {
+    return param;
+  }
+}
